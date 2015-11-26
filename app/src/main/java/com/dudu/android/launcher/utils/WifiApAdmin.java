b440a361230e5e3c66a628f6b7f5f8045e0e1115
sean <sean.du@dudu.tm>
2015-11-26 19:41:29 +0800

    add wifi init log
    Change-Id: I7fbbda894a3b7e186abc212f43a8f4fb2e5a24b7

--- app/src/main/java/com/dudu/android/launcher/utils/WifiApAdmin.java
+++ app/src/main/java/com/dudu/android/launcher/utils/WifiApAdmin.java
@@ -1,19 +1,21 @@
 package com.dudu.android.launcher.utils;
 
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.reflect.Method;
-
 import android.content.Context;
 import android.net.wifi.WifiConfiguration;
 import android.net.wifi.WifiManager;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Method;
+import java.util.concurrent.TimeUnit;
+
+import rx.functions.Action1;
 
 public class WifiApAdmin {
-
-    public static final String TAG = "WifiApAdmin";
-
     public static final String KEY_WIFI_AP_STATE = "wifi_ap_state";
     public static final String KEY_WIFI_AP_SSID = "wifi_ssid";
     public static final String KEY_WIFI_AP_PASSWORD = "wifi_password";
@@ -25,6 +27,7 @@ public class WifiApAdmin {
     private static final String WIFI_CONF_NAME = "nodogsplash.conf";
 
     private static WifiManager mWifiManager = null;
+    private static Logger log_init = LoggerFactory.getLogger("init.wifi.ap");
 
     /**
      * 设置WIFI热点密码
@@ -32,17 +35,35 @@ public class WifiApAdmin {
      * @param password
      */
     public static boolean setWifiApPassword(Context context, String password) {
+        log_init.debug("热点:设置密码");
         SharedPreferencesUtil.putStringValue(context, KEY_WIFI_AP_PASSWORD, password);
         return startWifiAp(context, SharedPreferencesUtil.getStringValue(context,
                 KEY_WIFI_AP_SSID, DEFAULT_SSID), password, null);
     }
 
-    private static boolean checkWifiConfigFile(Context context) {
+    private static boolean isWifiConfiged(Context context) {
         File dir = new File(FileUtils.getExternalStorageDirectory(), WIFI_CONF_DIRECTORY);
         if (!dir.exists()) {
             dir.mkdirs();
         }
 
+        File file = new File(dir, WIFI_CONF_NAME);
+        if (!file.exists()) {
+            log_init.debug("热点配置文件不存在");
+            return false;
+        }
+
+        log_init.debug("热点配置文件存在");
+        return true;
+    }
+
+    private static boolean initWifiConfig(Context context) {
+        File dir = new File(FileUtils.getExternalStorageDirectory(), WIFI_CONF_DIRECTORY);
+        if (!dir.exists()) {
+            dir.mkdirs();
+        }
+
+        log_init.debug("创建热点配置文件");
         File file = new File(dir, WIFI_CONF_NAME);
         if (!file.exists()) {
             try {
@@ -52,7 +73,7 @@ public class WifiApAdmin {
 
                 FileUtils.copyFileToSd(isAsset, file);
             } catch (IOException e) {
-                LogUtils.e(TAG, e.getMessage() + "");
+                log_init.debug("创建热点配置文件：失败", e);
                 return false;
             }
         }
@@ -60,25 +81,44 @@ public class WifiApAdmin {
         return true;
     }
 
-    public static boolean initWifiApState(Context context) {
-        if (!checkWifiConfigFile(context)) {
-            return false;
-        }
-
-        if (Utils.isTaxiVersion()) {
-            startWifiAp(context, DEFAULT_SSID, "", null);
+    public static boolean initWifiApState(final Context context) {
+        log_init.debug("热点初始化");
+        if (isWifiConfiged(context)) {
+            //如果开机已经打开ap，则不再打开
+            if (isWifiApEnabled(context)) {
+                log_init.debug("热点已经打开");
+                return true;
+            }
         } else {
-            if (SharedPreferencesUtil.getBooleanValue(context, KEY_WIFI_AP_STATE, false)) {
-                String ssid = SharedPreferencesUtil.getStringValue(context, KEY_WIFI_AP_SSID, DEFAULT_SSID);
-                String password = SharedPreferencesUtil.getStringValue(context, KEY_WIFI_AP_PASSWORD, DEFAULT_PASSWORD);
-                startWifiAp(context, ssid, password, null);
+            initWifiConfig(context);
+            if (isWifiApEnabled(context)) {
+                log_init.debug("热点已经打开，需要先关闭");
+                closeWifiAp(context);
             }
         }
+
+        log_init.debug("5s后打开热点");
+        rx.Observable.timer(5, TimeUnit.SECONDS)
+                .subscribe(new Action1<Long>() {
+                    @Override
+                    public void call(final Long aLong) {
+                        if (Utils.isTaxiVersion()) {
+                            startWifiAp(context, DEFAULT_SSID, "", null);
+                        } else {
+                            if (SharedPreferencesUtil.getBooleanValue(context, KEY_WIFI_AP_STATE, false)) {
+                                String ssid = SharedPreferencesUtil.getStringValue(context, KEY_WIFI_AP_SSID, DEFAULT_SSID);
+                                String password = SharedPreferencesUtil.getStringValue(context, KEY_WIFI_AP_PASSWORD, DEFAULT_PASSWORD);
+                                startWifiAp(context, ssid, password, null);
+                            }
+                        }
+                    }
+                });
 
         return true;
     }
 
     public static boolean startWifiAp(Context context) {
+        log_init.debug("读取配置，然后打开热点");
         String ssid = SharedPreferencesUtil.getStringValue(context, KEY_WIFI_AP_SSID, DEFAULT_SSID);
         String password = SharedPreferencesUtil.getStringValue(context, KEY_WIFI_AP_PASSWORD, DEFAULT_PASSWORD);
         return startWifiAp(context, ssid, password, null);
@@ -89,8 +129,6 @@ public class WifiApAdmin {
         mWifiManager = (WifiManager) context
                 .getSystemService(Context.WIFI_SERVICE);
 
-        closeWifiAp(context);
-
         if (mWifiManager.isWifiEnabled()) {
             mWifiManager.setWifiEnabled(false);
         }
@@ -99,6 +137,7 @@ public class WifiApAdmin {
             return false;
         }
 
+        log_init.debug("打开热点成功，保存状态");
         SharedPreferencesUtil.putBooleanValue(context, KEY_WIFI_AP_STATE, true);
 
         if (callback != null) {
@@ -109,23 +148,26 @@ public class WifiApAdmin {
     }
 
     private static boolean startWifiAp(String ssid, String password) {
+        log_init.debug("反射打开热点{}", ssid);
         Method method;
         try {
             method = mWifiManager.getClass().getMethod("setWifiApEnabled",
                     WifiConfiguration.class, boolean.class);
             WifiConfiguration netConfig = new WifiConfiguration();
             netConfig.SSID = ssid;
-            if (password.isEmpty()) {
-//                netConfig.allowedAuthAlgorithms.clear();
-//                netConfig.allowedGroupCiphers.clear();
-//                netConfig.allowedKeyManagement.clear();
-//                netConfig.allowedPairwiseCiphers.clear();
-//                netConfig.allowedProtocols.clear();
-//                netConfig.wepKeys[0] = "";
-//                netConfig.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
-//                netConfig.wepTxKeyIndex = 0;
-                netConfig = null;
+            if (password == null || (password != null && password.isEmpty())) {
+                log_init.debug("没有密码，清除掉配置");
+                netConfig.allowedAuthAlgorithms.clear();
+                netConfig.allowedProtocols.clear();
+                netConfig.allowedKeyManagement.clear();
+                netConfig.allowedKeyManagement.set(WifiConfiguration.KeyMgmt.NONE);
+                netConfig.allowedPairwiseCiphers.clear();
+                netConfig.allowedGroupCiphers.clear();
+
+                netConfig.wepKeys[0] = "";
+                netConfig.wepTxKeyIndex = 0;
             } else {
+                log_init.debug("有密码，设置配置");
                 netConfig.preSharedKey = password;
                 netConfig.allowedAuthAlgorithms
                         .set(WifiConfiguration.AuthAlgorithm.OPEN);
@@ -145,13 +187,14 @@ public class WifiApAdmin {
 
             return (Boolean) method.invoke(mWifiManager, netConfig, true);
         } catch (Exception e) {
-            LogUtils.e(TAG, e.getMessage() + "");
+            log_init.debug("反射打开热点异常", e);
         }
 
         return false;
     }
 
     public static void closeWifiAp(Context context) {
+        log_init.debug("反射关闭热点");
         if (isWifiApEnabled(context)) {
             mWifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
             Method setWifiApEnabled;
@@ -161,12 +204,13 @@ public class WifiApAdmin {
                 setWifiApEnabled.invoke(mWifiManager, null, false);
                 SharedPreferencesUtil.putBooleanValue(context, KEY_WIFI_AP_STATE, false);
             } catch (Exception e) {
-                LogUtils.e(TAG, e.getMessage() + "");
+                log_init.debug("反射关闭热点异常", e);
             }
         }
     }
 
     public static boolean isWifiApEnabled(Context context) {
+        log_init.debug("反射查询热点");
         try {
             if (mWifiManager == null) {
                 mWifiManager = (WifiManager) context
@@ -177,7 +221,7 @@ public class WifiApAdmin {
             method.setAccessible(true);
             return (Boolean) method.invoke(mWifiManager);
         } catch (Exception e) {
-            LogUtils.e(TAG, e.getMessage() + "");
+            log_init.debug("反射查询热点异常", e);
         }
 
         return false;
