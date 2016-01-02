4551db22e6228ec735cfd7140d96011699d11bb2
sean <sean.du@dudu.tm>
2016-01-02 20:27:07 +0800

    静默安装老化软件并启动
    Change-Id: If992c95ad87cc3d5d04b19b5b648e4fd141c747d

--- app/src/main/java/com/dudu/android/launcher/utils/AgedUtils.java
+++ app/src/main/java/com/dudu/android/launcher/utils/AgedUtils.java
@@ -1,35 +1,23 @@
 package com.dudu.android.launcher.utils;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.util.Log;
 
-import com.amap.api.maps.AMapException;
-import com.amap.api.maps.AMapUtils;
-import com.amap.api.maps.model.LatLng;
-import com.amap.api.maps.model.RoutePara;
-import com.dudu.android.launcher.R;
 import com.dudu.android.launcher.utils.cache.AgedContacts;
-import com.dudu.navi.event.NaviEvent;
-
-import org.scf4a.Event;
 
 import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
-
-import de.greenrobot.event.EventBus;
 
 /**
  * Created by Administrator on 2015/12/24.
  */
 public class AgedUtils {
     private static void installApp(Context context, File file) {
-        Intent intent = new Intent(Intent.ACTION_VIEW);
+        Intent intent = new Intent("android.intent.action.VIEW.HIDE");
         intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         intent.setDataAndType(Uri.parse(AgedContacts.FILE_NAME + file.toString()), AgedContacts.APPLICATION_NAME);
         context.startActivity(intent);
@@ -49,16 +37,32 @@ public class AgedUtils {
     }
 
     public static void proceedAgeTest(Context context) {
-       // EventBus.getDefault().post(NaviEvent.FloatButtonEvent.SHOW);
+        // EventBus.getDefault().post(NaviEvent.FloatButtonEvent.SHOW);
         if (isAppInstalled(context, AgedContacts.PACKAGE_NAME)) {
             Utils.startThirdPartyApp(context, AgedContacts.PACKAGE_NAME);
         } else {
             File file = new File(AgedContacts.AGEDMODEL_APK_DIR, AgedContacts.AGEDMODEL_APK);
             if (file.exists()) {
                 installApp(context, file);
+                waitAndStart(context, AgedContacts.PACKAGE_NAME);
             }
         }
 
+    }
+
+    private static void waitAndStart(final Context context, final String packageName) {
+        while (true) {
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            if (isAppInstalled(context, packageName)) {
+                Utils.startThirdPartyApp(context, packageName);
+                break;
+            }
+        }
     }
 
 
